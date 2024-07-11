328f3c88111feb59f141f1b0d8ac35e1e0d88c35
Artemisa Ruci <artemisa.ruci@gmail.com>
2024-07-11 16:01:42 +0200

    Populate Display on Number Click

--- main.js
+++ main.js
@@ -3,6 +3,31 @@ const firstNum = 2;
 const secondNum = 3;
 const operator = "+";
 
+// Buttons
+const operandButtons = document.querySelectorAll("button.operand");
+const operatorButtons = document.querySelectorAll("button.operator");
+const clearBtn = document.querySelector("#clear");
+
+// Display
+const display = document.querySelector("#display");
+
+// Store Value Input
+
+
+// Put Number Value on Display
+function putDisplayVal(e) {
+    const operand = e.target.textContent;
+    if (display.textContent == "0") {
+        display.textContent = "";
+    }
+    display.textContent += operand;
+}
+
+// Reset Display Value
+function resetDisplayVal() {
+    display.textContent = "0";
+}
+
 // Basic Math Operations
 function add() {
     let sum = firstNum + secondNum;
@@ -27,3 +52,10 @@ function operate() {
     if (operator == "*") {multiply()};
     if (operator == "/") {divide()};
 }
+
+// Event Listeners
+operandButtons.forEach(operandButton => {
+    operandButton.addEventListener("click", (e) => {putDisplayVal(e)})
+})
+
+clearBtn.addEventListener("click", () => {resetDisplayVal()})
