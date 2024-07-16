279fc33f73c429bc515b619a63c0698ebb572c68
Artemisa Ruci <artemisa.ruci@gmail.com>
2024-07-16 15:25:16 +0200

    Get Operation Values

--- main.js
+++ main.js
@@ -1,83 +1,123 @@
 // General Variables
-const firstNum = 2;
-const secondNum = 3;
-const result = 5;
-//const operator = operandButtons.textContent;
+let firstOperand = null;
+let secondOperand = null;
+let firstOperator = null;
+let secondOperator = null;
+let result = null;
 let displayVal = "";
-let displayTxt = "0";
-
-
 
 // Buttons
-const operandButtons = document.querySelectorAll("button.operand");
-const operatorButtons = document.querySelectorAll("button.operator");
-const clearBtn = document.querySelector("#clear");
-const signBtn = document.querySelector("#sign");
-const percentageBtn = document.querySelector("#percentage");
+const buttons = document.querySelectorAll("button");
 
 // Display
 const display = document.querySelector("#display");
 
-// S
-function getOperandVal(e) {
-    const btnClass = e.target.classList;
+// Make the Calculator Work
+function operate(e) {
+    const buttonClass = e.target.classList;
+    const buttonId = e.target.id;
     const userInput = e.target.textContent;
-    if (btnClass.contains("operand")) {
-        for (let i = 0; i <= 0; i++) {
-            putDisplayVal(userInput);
-            if (btnClass.contains("operator")) {
-                break;
-            }
-        }
-        
+
+    if (buttonClass.contains("operand")) {
+        getDisplayValue(userInput);
+        putDisplayValue();
+        //getOperationValues();
+    } else if (buttonClass.contains("operator")) {
+        getOperationValues(userInput);
+        putDisplayValue();
+    } else if (buttonId == "result") {
+        //test();
+        //putDisplayValue();
+    } else if (buttonId == "clear") {
+
+    } else if (buttonId == "sign") {
+
+    } else if (buttonId == "percentage") {
+
+    } else if (buttonId == "decimal") {
+
     }
 }
 
+// Get Display Value from Number Input
+function getDisplayValue(num) {
+    if (displayVal == firstOperand) {
+        displayVal = num
+    } else {
+        displayVal += num;
+    }
+}
 
 // Put Number Value on Display
-function putDisplayVal(num) {
-    //const input = e.target.textContent;
-    displayVal += num;
-    displayTxt = displayVal;
-    display.textContent = displayTxt;
-    return displayTxt;
+function putDisplayValue() {
+    display.textContent = displayVal;
+}
+
+// Get Operands and Operators from Operator Input
+function getOperationValues(operator) {
+    if (firstOperator != null && secondOperator == null) {
+        secondOperand = displayVal;
+        secondOperator = operator;
+        calculate(firstOperator);
+        displayVal = result.toString();
+        firstOperand = displayVal;
+        secondOperand = null;
+        result = null;
+        console.log(`${firstOperand}, ${firstOperator}, ${secondOperand}, ${secondOperator}, ${result}`);
+    } else if (firstOperator != null && secondOperator != null) {
+        secondOperand = displayVal;
+        firstOperator = operator;
+        calculate(secondOperator);
+        displayVal = result.toString();
+        firstOperand = displayVal;
+        secondOperand = null;
+        secondOperator = null;
+        result = null;
+        console.log(`${firstOperand}, ${firstOperator}, ${secondOperand}, ${secondOperator}, ${result}`);
+    } else {
+        firstOperand = displayVal;
+        firstOperator = operator;
+        console.log(`${firstOperand}, ${firstOperator}, ${secondOperand}, ${secondOperator}, ${result}`);
+    }
 }
 
 // Reset Display Value
-function resetDisplayVal() {
-    displayTxt = "0";
-    displayVal = "";
-    display.textContent = displayTxt;
-}
 
 // Basic Math Operations
 function add() {
-    let sum = firstNum + secondNum;
+    let sum = Number(firstOperand) + Number(secondOperand);
     return sum;
 }
 function substract() {
-    let substraction = firstNum - secondNum;
+    let substraction = Number(firstOperand) - Number(secondOperand);
     return substraction;
 }
 function multiply() {
-    let multiplication = firstNum * secondNum;
+    let multiplication = Number(firstOperand) * Number(secondOperand);
     return multiplication;
 }
 function divide() {
-    let division = firstNum / secondNum;
+    let division = Number(firstOperand) / Number(secondOperand);
     return division;
 }
 
-function operate() {
-    if (operator === "+") {add()};
-    if (operator == "-") {substract()};
-    if (operator == "*") {multiply()};
-    if (operator == "/") {divide()};
+function calculate(operator) {
+    if (operator == "+") {
+        result = add();
+        return result;
+    } else if (operator == "-") {
+        result = substract();
+        return result;
+    } else if (operator == "*") {
+        result = multiply();
+        return result;
+    } else if (operator == "/") {
+        result = divide();
+        return result;
+    }
 }
 
 // Event Listeners
-operandButtons.forEach(operandButton => {
-    operandButton.addEventListener("click", (e) => {getOperandVal(e)})
+buttons.forEach(button => {
+    button.addEventListener("click", (e) => {operate(e)})
 })
-
-clearBtn.addEventListener("click", () => {resetDisplayVal()})
